a72a7e026f18bb37506905ab4e959c6a20a695a0
Damjan <damjandimitrov1@gmail.com>
2019-12-02 22:04:34 +0100

    Event handlers moved to new class

---
+++ src/Helpers/EventHandlers.java
@@ -0,0 +1,52 @@
+package Helpers;
+
+import Logic.RPSLS;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class EventHandlers
+{
+	
+	JavaFX_Helper helper = new JavaFX_Helper();
+	
+	
+	protected EventHandler<ActionEvent> openNewWindow()
+	{
+		return event ->
+		{
+			Stage stage = new Stage();
+			stage.setTitle("My New Stage Title");
+			stage.setScene(new Scene(new Button("click"), 600, 700));
+			stage.show();
+			((Node) event.getSource()).getScene().getWindow().hide();
+		};
+	}
+
+	protected EventHandler<ActionEvent> howToPlayWindow()
+	{
+		return event ->
+		{
+			Stage stage = new Stage();
+			stage.setTitle("How to play");
+			GridPane gridPane = helper.createGridPane();
+			Scene scene = new Scene(gridPane, 988, 550);
+			stage.setScene(scene);
+			stage.setResizable(false);
+			stage.show();
+			((Node) event.getSource()).getScene().getWindow().hide();
+			stage.setOnCloseRequest(closeWindowEvent());
+			helper.addInstructionWindowItems(gridPane);
+		};
+	}
+
+	protected EventHandler<WindowEvent> closeWindowEvent()
+	{
+		return event -> RPSLS.showStartScreen(new Stage());
+	}
+}
--- src/Helpers/JavaFX_Helper.java
+++ src/Helpers/JavaFX_Helper.java
@@ -1,13 +1,8 @@
 package Helpers;
 
-import Logic.RPSLS;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -24,11 +19,10 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.TextAlignment;
-import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 
 public class JavaFX_Helper
 {
+	EventHandlers eventHandler = new EventHandlers();
 	public GridPane createGridPane()
 	{
 		GridPane gridPane = new GridPane();
@@ -57,17 +51,17 @@ public class JavaFX_Helper
 
 		Button playButton = makeButton("Play");
 		gridPane.add(playButton, 0, 2);
-		playButton.setOnAction(openNewWindow());
+		playButton.setOnAction(eventHandler.openNewWindow());
 		GridPane.setHalignment(playButton, HPos.CENTER);
 
 		Button testButton = makeButton("Test ");
 		gridPane.add(testButton, 0, 3);
-		testButton.setOnAction(openNewWindow());
+		testButton.setOnAction(eventHandler.openNewWindow());
 		GridPane.setHalignment(testButton, HPos.CENTER);
 
 		Button howToPlay = makeButton("How to play ");
 		gridPane.add(howToPlay, 0, 4);
-		howToPlay.setOnAction(howToPlayWindow());
+		howToPlay.setOnAction(eventHandler.howToPlayWindow());
 		GridPane.setHalignment(howToPlay, HPos.CENTER);
 	}
 
@@ -110,40 +104,6 @@ public class JavaFX_Helper
 		text.setBackground(new Background(new BackgroundFill(Color.rgb(20, 19, 60), CornerRadii.EMPTY, Insets.EMPTY)));
 		gridPane.add(text, 1, 0);
 
-	}
-
-	protected EventHandler<ActionEvent> openNewWindow()
-	{
-		return event ->
-		{
-			Stage stage = new Stage();
-			stage.setTitle("My New Stage Title");
-			stage.setScene(new Scene(new Button("click"), 600, 700));
-			stage.show();
-			((Node) event.getSource()).getScene().getWindow().hide();
-		};
-	}
-
-	protected EventHandler<ActionEvent> howToPlayWindow()
-	{
-		return event ->
-		{
-			Stage stage = new Stage();
-			stage.setTitle("How to play");
-			GridPane gridPane = createGridPane();
-			Scene scene = new Scene(gridPane, 988, 550);
-			stage.setScene(scene);
-			stage.setResizable(false);
-			stage.show();
-			((Node) event.getSource()).getScene().getWindow().hide();
-			stage.setOnCloseRequest(closeWindowEvent());
-			addInstructionWindowItems(gridPane);
-		};
-	}
-
-	protected EventHandler<WindowEvent> closeWindowEvent()
-	{
-		return event -> RPSLS.showStartScreen(new Stage());
 	}
 
 	private String getInstructions()
