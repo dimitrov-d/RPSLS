43225983f6446c2f79e8a569f68a7a07e67d853e
Damjan <damjandimitrov1@gmail.com>
2019-12-06 18:45:58 +0100

    Window Items methods moved to new class

--- src/Helpers/JavaFX_Helper.java
+++ src/Helpers/JavaFX_Helper.java
@@ -3,7 +3,6 @@ package Helpers;
 import java.util.Arrays;
 import java.util.Optional;
 
-import Logic.*;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -28,10 +27,8 @@ import javafx.scene.text.TextAlignment;
 public class JavaFX_Helper
 {
 
-	Player[] player = new Player[5];
-	static TextInputDialog dialog = new TextInputDialog("5");
 
-	public GridPane createGridPane()
+	public static GridPane createGridPane()
 	{
 		GridPane gridPane = new GridPane();
 		gridPane.setAlignment(Pos.CENTER);
@@ -41,39 +38,8 @@ public class JavaFX_Helper
 		return gridPane;
 	}
 
-	public void addMainWindowItems(GridPane gridPane)
-	{
-		Label headerLabel = new Label("Welcome To");
-		headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 36));
-		headerLabel.setTextFill(Color.ANTIQUEWHITE);
-		gridPane.add(headerLabel, 0, 0);
-		GridPane.setHalignment(headerLabel, HPos.CENTER);
-		GridPane.setMargin(headerLabel, new Insets(0, 0, 0, 0));
 
-		Label nameLabel = new Label("Rock Paper Scissors Lizard Spock");
-		nameLabel.setFont(Font.font("Arial", FontWeight.BOLD, 30));
-		nameLabel.setTextFill(Color.ANTIQUEWHITE);
-		gridPane.add(nameLabel, 0, 1);
-		GridPane.setHalignment(nameLabel, HPos.CENTER);
-		GridPane.setMargin(nameLabel, new Insets(-50, 0, 50, 0));
-
-		Button playButton = makeButton("Play");
-		gridPane.add(playButton, 0, 2);
-		playButton.setOnAction(new EventHandlers().openNewWindow());
-		GridPane.setHalignment(playButton, HPos.CENTER);
-
-		Button testButton = makeButton("Test ");
-		gridPane.add(testButton, 0, 3);
-		testButton.setOnAction(new EventHandlers().testWindow());
-		GridPane.setHalignment(testButton, HPos.CENTER);
-
-		Button howToPlay = makeButton("How to play ");
-		gridPane.add(howToPlay, 0, 4);
-		howToPlay.setOnAction(new EventHandlers().howToPlayWindow());
-		GridPane.setHalignment(howToPlay, HPos.CENTER);
-	}
-
-	protected Button makeButton(String text)
+	protected static Button makeButton(String text)
 	{
 		Button button = new Button(text);
 		button.setPrefHeight(60);
@@ -83,7 +49,7 @@ public class JavaFX_Helper
 		return button;
 	}
 
-	protected Background getBackground()
+	protected static Background getBackground()
 	{
 		BackgroundImage woodBackground = new BackgroundImage(
 				new Image("https://image.freepik.com/free-photo/old-wooden-texture-background-vintage_55716-1138.jpg",
@@ -93,42 +59,14 @@ public class JavaFX_Helper
 		return new Background(woodBackground);
 	}
 
-	protected void addInstructionWindowItems(GridPane gridPane)
-	{
-		var image = new Image(
-				"https://d3qdvvkm3r2z1i.cloudfront.net/media/catalog/product/cache/1/image/1800x/6b9ffbf72458f4fd2d3cb995d92e8889/r/o/rockpaperscissorslizardspock_newthumb.png",
-				400, 550, false, true);
-		ImageView imageView = new ImageView(image);
-		gridPane.add(imageView, 0, 0);
-		String instructions = getInstructions();
-		Label text = new Label(instructions);
-		text.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-		text.setTextFill(Color.rgb(95, 197, 185));
-		GridPane.setHalignment(text, HPos.CENTER);
-		GridPane.setMargin(text, new Insets(0, 0, 0, -10));
-		text.setTextAlignment(TextAlignment.CENTER);
-		text.setPrefHeight(600);
-		text.setStyle("-fx-line-spacing: 0.5em;");
-		text.setBackground(new Background(new BackgroundFill(Color.rgb(20, 19, 60), CornerRadii.EMPTY, Insets.EMPTY)));
-		gridPane.add(text, 1, 0);
 
-	}
+	
 
-	protected void addTestWindowItems(GridPane gridPane)
-	{
-		dialog.setTitle("Input");
-		dialog.setHeaderText("How many games should each player play?");
-		dialog.setContentText("Enter here:");
-
-		Optional<String> numGames = dialog.showAndWait();
-		numGames = validateInput(numGames);
-		initiatePlayers(player, Integer.parseInt(numGames.get()));
-	}
-
-	private String getInstructions()
+	static String getInstructions()
 	{
 		return "The game is an extension of the game Rock, Paper, Scissors. \n" + "Every player picks an element. \n"
-				+ "Every player plays the given amount of games with each other player \n"
+				+ "Every player plays the given amount \n"
+				+ "of games with each other player. \n"
 				+ "The winner is the player who beats the other players. \n"
 				+ "In case of a tie, the game repeats until we find a winner. \n \n"
 				+ "The rules of the game are as follows: \n" + "- Scissors cuts Paper \n" + "- Paper covers Rock \n"
@@ -137,28 +75,5 @@ public class JavaFX_Helper
 				+ "- (and as it always has) Rock crushes Scissors \n";
 	}
 
-	private static void initiatePlayers(Player[] player, int numGames)
-	{
-		for (int i = 0; i < player.length; i++)
-		{
-			player[i] = new Player();
-		}
-
-		Logic.playGame(player, numGames);
-
-		System.out.println("Ties: " + Logic.getTies());
-		Arrays.asList(player).stream().forEach(p -> System.out.println(p.element + ": " + p.score));
-	}
-
-	private static Optional<String> validateInput(Optional<String> input)
-	{
-		while (!input.get().matches("^\\d+$")) // Only numbers and non-empty input
-		{
-			dialog.setHeaderText("Please enter a number!");
-			input = dialog.showAndWait();
-		}
-
-		input.ifPresent(games -> System.out.println("Number of games per player: " + games));
-		return input;
-	}
+	
 }
---
+++ src/Helpers/WindowItems.java
@@ -0,0 +1,118 @@
+package Helpers;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import Logic.Logic;
+import Logic.Player;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
+
+public class WindowItems
+{
+	Player[] player = new Player[5];
+	static TextInputDialog dialog = new TextInputDialog("5");
+	
+	public static void addMainWindowItems(GridPane gridPane)
+	{
+		Label headerLabel = new Label("Welcome To");
+		headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 36));
+		headerLabel.setTextFill(Color.ANTIQUEWHITE);
+		gridPane.add(headerLabel, 0, 0);
+		GridPane.setHalignment(headerLabel, HPos.CENTER);
+		GridPane.setMargin(headerLabel, new Insets(0, 0, 0, 0));
+
+		Label nameLabel = new Label("Rock Paper Scissors Lizard Spock");
+		nameLabel.setFont(Font.font("Arial", FontWeight.BOLD, 30));
+		nameLabel.setTextFill(Color.ANTIQUEWHITE);
+		gridPane.add(nameLabel, 0, 1);
+		GridPane.setHalignment(nameLabel, HPos.CENTER);
+		GridPane.setMargin(nameLabel, new Insets(-50, 0, 50, 0));
+
+		Button playButton = JavaFX_Helper.makeButton("Play");
+		gridPane.add(playButton, 0, 2);
+		playButton.setOnAction(new EventHandlers().openNewWindow());
+		GridPane.setHalignment(playButton, HPos.CENTER);
+
+		Button testButton = JavaFX_Helper.makeButton("Test ");
+		gridPane.add(testButton, 0, 3);
+		testButton.setOnAction(new EventHandlers().testWindow());
+		GridPane.setHalignment(testButton, HPos.CENTER);
+
+		Button howToPlay = JavaFX_Helper.makeButton("How to play ");
+		gridPane.add(howToPlay, 0, 4);
+		howToPlay.setOnAction(new EventHandlers().howToPlayWindow());
+		GridPane.setHalignment(howToPlay, HPos.CENTER);
+	}
+	
+	protected void addInstructionWindowItems(GridPane gridPane)
+	{
+		Image image = new Image(
+				"https://d3qdvvkm3r2z1i.cloudfront.net/media/catalog/product/cache/1/image/1800x/6b9ffbf72458f4fd2d3cb995d92e8889/r/o/rockpaperscissorslizardspock_newthumb.png",
+				400, 580, false, true);
+		ImageView imageView = new ImageView(image);
+		gridPane.add(imageView, 0, 0);
+		String instructions = JavaFX_Helper.getInstructions();
+		Label text = new Label(instructions);
+		text.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+		text.setTextFill(Color.rgb(95, 197, 185));
+		GridPane.setHalignment(text, HPos.CENTER);
+		GridPane.setMargin(text, new Insets(0, 0, 0, -10));
+		text.setTextAlignment(TextAlignment.CENTER);
+		text.setPrefHeight(600);
+		text.setStyle("-fx-line-spacing: 0.5em;");
+		text.setBackground(new Background(new BackgroundFill(Color.rgb(20, 19, 60), CornerRadii.EMPTY, Insets.EMPTY)));
+		gridPane.add(text, 1, 0);
+
+	}
+	
+	protected void addTestWindowItems(GridPane gridPane)
+	{
+		dialog.setTitle("Input");
+		dialog.setHeaderText("How many games should each player play?");
+		dialog.setContentText("Enter here:");
+
+		Optional<String> numGames = dialog.showAndWait();
+		numGames = validateInput(numGames);
+		initiatePlayers(player, Integer.parseInt(numGames.get()));
+	}
+	
+	private static void initiatePlayers(Player[] player, int numGames)
+	{
+		for (int i = 0; i < player.length; i++)
+		{
+			player[i] = new Player();
+		}
+
+		Logic.playGame(player, numGames);
+
+		System.out.println("Ties: " + Logic.getTies());
+		Arrays.asList(player).stream().forEach(p -> System.out.println(p.element + ": " + p.score));
+	}
+
+	private static Optional<String> validateInput(Optional<String> input)
+	{
+		while (!input.get().matches("^\\d+$")) // Only numbers and non-empty input
+		{
+			dialog.setHeaderText("Please enter a number!");
+			input = dialog.showAndWait();
+		}
+
+		input.ifPresent(games -> System.out.println("Number of games per player: " + games));
+		return input;
+	}
+
+}
