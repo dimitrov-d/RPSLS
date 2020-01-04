6986e089d20afccccd039502b2aae0f54b8b7b00
Damjan <damjandimitrov1@gmail.com>
2020-01-04 20:38:28 +0100

    Program requires network to run game

--- src/Logic/Main.java
+++ src/Logic/Main.java
@@ -1,10 +1,17 @@
 package Logic;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import Utility.*;
 import Windows.MainWindow;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -13,7 +20,11 @@ public class Main extends Application
 	@Override
 	public void start(Stage primaryStage) throws Exception
 	{
-		showStartScreen(primaryStage);
+		if (netIsAvailable())
+			showStartScreen(primaryStage);
+		else
+			showNoConnectionWarning();
+
 	}
 
 	public static void showStartScreen(Stage primaryStage)
@@ -31,5 +42,31 @@ public class Main extends Application
 	public static void main(String[] args)
 	{
 		launch(args);
+	}
+
+	private static boolean netIsAvailable()
+	{
+		try
+		{
+			// Custom URL for testing
+			final URLConnection connection = new URL("http://www.google.com").openConnection();
+			connection.connect();
+			connection.getInputStream().close();
+			return true;
+		}
+		catch (MalformedURLException e)
+		{
+			throw new RuntimeException(e);
+		}
+		catch (IOException e)
+		{
+			return false;
+		}
+	}
+	
+	private static void showNoConnectionWarning()
+	{
+		Alert alert = new Alert(AlertType.WARNING, "Please connect to the internet and try again", ButtonType.OK);
+		alert.showAndWait();
 	}
 }
